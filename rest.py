edd4933f22f679e30c4799d4bb349ff4238f2ed2
xuejun <xuejun0131@gmail.com>
2019-09-23 17:11:54 +0800

    fix(time):修复显示位数的bug

--- rest.py
+++ rest.py
@@ -10,7 +10,7 @@ while True:
     print("\nThis program started on: " + time.ctime())
     while time.time() < start + min * 60:
         last_time = time.time() - start
-        print(str(round(last_time / (min * 60), 4))+'%', end='\r')
+        print(str(round(last_time*100 / (min * 60), 4))+'%', end='\r')
     print("\nBreak Time!\t" + time.ctime())
     webbrowser.open("http://106.14.139.144/articles/2019/09/05/1567664492948.html")
     flag = input('continue to remind you?\n')
