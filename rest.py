d7801495127e7dc05acdf71ec29b40beb17b9090
xuejun <xuejun0131@gmail.com>
2019-09-23 16:21:56 +0800

    feat(time):增加时间流逝百分比显示

--- rest.py
+++ rest.py
@@ -4,11 +4,16 @@ import time
 print("The reminder started!")
 flag = 0
 while True: 
+    start = time.time()
+    min = 25
+
     print("\nThis program started on: " + time.ctime())
-    time.sleep(25 * 60)
+    while time.time() < start + min * 60:
+        last_time = time.time() - start
+        print(str(round(last_time / (min * 60), 4))+'%', end='\r')
     print("\nBreak Time!\t" + time.ctime())
     webbrowser.open("http://106.14.139.144/articles/2019/09/05/1567664492948.html")
-    flag = input('continue to remind you?')
+    flag = input('continue to remind you?\n')
     if flag == 'yes':
         continue
     else:
